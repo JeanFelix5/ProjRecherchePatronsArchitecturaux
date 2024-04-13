e2d60d7d63dd8811371bce71c1395f6a04a8e91d
JeanFelix5 <jf_girard5@hotmail.ca>
2024-04-13 15:24:23 -0400

    Bug fix

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits> 
 #include "./PeerToPeer/peer_network.h"
 #include "MVC/view.h"
 #include "MVC/controller.h"
@@ -85,50 +86,60 @@ void ECSExample() {
 
 
 int main() {
-    int choice;
+    int choice = -1;
+
     do {
         // Affichage du menu
         cout << "Choisissez un exemple a executer :" << endl;
         cout << "1. Exemple 1 : Modele-Vue-Controleur pour l'authentification" << endl;
-        cout << "2. Exemple 2 : Creation d evennement C++" << endl;
+        cout << "2. Exemple 2 : Creation d'evennement C++" << endl;
         cout << "3. Exemple 3 : Connection Via Broker" << endl;
         cout << "4. Exemple 4 : Pipe and Filter Architecture pour le traitement d'une phrase" << endl;
         cout << "5. Exemple 5 : Deplacement d'un GameObject genere par le programme" << endl;
         cout << "6. Exemple 6 : Reseau peer-to-peer basique" << endl;
         cout << "0. Quitter" << endl;
         cout << "Votre choix : ";
-        cin >> choice;
 
-        // Execution de l'exemple choisi
-        switch(choice)
-        {
-        case 1:
-            ConnectToWebSite();
-            break;
-        case 2:
-            SubscribeToEvent();
-            break;
-        case 3:
-            ConnectViaBroker();
-            break;
-        case 4:
-            executePipeAndFilter();
-            break;
-        case 5:
-            ECSExample();
-            break;
-        case 6:
-            ConnectMultiplePeers();
-            break;
-        case 0:
-            cout << "Au revoir !" << endl;
-            break;
-        default:
-            cout << "Choix invalide. Veuillez reessayer." << endl;
-            break;
+        
+        if (!(cin >> choice)) {
+            cout << "Erreur : Veuillez entrer un nombre." << endl;
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            choice = -1; 
+        }
+        else if (choice < 0 || choice > 6) {
+            cout << "Choix invalide. Veuillez tenter de nouveau." << endl;
+            choice = -1; // Reset choice to an uninitialized state
+        }
+        else {
+            // Execution de l'exemple choisi
+            switch(choice)
+            {
+            case 1:
+                ConnectToWebSite();
+                break;
+            case 2:
+                SubscribeToEvent();
+                break;
+            case 3:
+                ConnectViaBroker();
+                break;
+            case 4:
+                executePipeAndFilter();
+                break;
+            case 5:
+                ECSExample();
+                break;
+            case 6:
+                ConnectMultiplePeers();
+                break;
+            case 0:
+                cout << "Au revoir !" << endl;
+                break;
+            }
         }
     } while(choice != 0);
-
+    
     return 0;
 }
 
